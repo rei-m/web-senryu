c0fb1dd399d0c2b50b11fe5808ffe54f1117617a
rei-m <star.oceans.rm@gmail.com>
2019-09-27 14:12:14 +0900

    update dependency fix routing

--- gatsby-node-impl/index.ts
+++ gatsby-node-impl/index.ts
@@ -4,18 +4,24 @@ export const onCreatePage: GatsbyOnCreatePage<{}> = async ({
   page,
   actions,
 }) => {
-  const { createPage } = actions;
-  if (page.path.match(/^\/senryu\/.*/)) {
-    if (page.path === '/senryu/' || page.path === '/senryu/new/') {
-      return;
+  const { createPage, deletePage } = actions;
+  if (page.path === '/create_senryu/') {
+    const oldPage = { ...page };
+    page.path = '/senryu/new/';
+    if (page.path !== oldPage.path) {
+      deletePage(oldPage);
+      createPage(page);
     }
-
-    page.matchPath = '/senryu/:id';
-
+    return;
+  }
+  if (page.path === '/senryu/show/') {
+    page.matchPath = '/senryu/:id/';
     createPage(page);
-  } else if (page.path.match(/^\/users\/.*/)) {
-    page.matchPath = '/users/:id';
-
+    return;
+  }
+  if (page.path === '/users/show/') {
+    page.matchPath = '/users/:id/';
     createPage(page);
+    return;
   }
 };
--- src/constants/routing.ts
+++ src/constants/routing.ts
@@ -1,7 +1,7 @@
 export const ROUTING = {
   root: '/',
   senryu: '/senryu/',
-  senryuNew: '/senryu/new/',
+  senryuNew: '/senryu/new/', // ライブラリverを上げたら /senryu/new/ のroutingが効かなくなったのでやむなく変更
   senryuShow: '/senryu/:id/',
   usersShow: '/users/:id/',
   about: '/about/',
