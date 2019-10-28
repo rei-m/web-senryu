88c97b424f3baf4fbdffd66a78bdf8ddbe5c0669
rei-m <star.oceans.rm@gmail.com>
2019-10-28 22:53:35 +0900

    add jest config

---
+++ src/utils/index.test.ts
@@ -0,0 +1,37 @@
+import { isBlankOrEmpty, isIncludeBlank } from './';
+
+describe('utility functions', () => {
+  describe('isBlankOrEmpty', () => {
+    it('should return true when blank', () => {
+      const actual = isBlankOrEmpty(' ');
+      expect(actual).toBeTruthy();
+    });
+
+    it('should return true when multibyte blank', () => {
+      const actual = isBlankOrEmpty('　');
+      expect(actual).toBeTruthy();
+    });
+
+    it('should return false when not blank', () => {
+      const actual = isBlankOrEmpty('a');
+      expect(actual).toBeFalsy();
+    });
+  });
+
+  describe('isIncludeBlank', () => {
+    it('should return true when blank', () => {
+      const actual = isIncludeBlank('a ');
+      expect(actual).toBeTruthy();
+    });
+
+    it('should return true when multibyte blank', () => {
+      const actual = isIncludeBlank('a　');
+      expect(actual).toBeTruthy();
+    });
+
+    it('should return false when not blank', () => {
+      const actual = isIncludeBlank('aa');
+      expect(actual).toBeFalsy();
+    });
+  });
+});
